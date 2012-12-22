e85134e7d1ff7fbcc8fb18ad8cbecfeaaac2dfff
Kieren Diment <diment@gmail.com>
2012-12-22 19:16:33 +1100

    reorganise docs

--- lib/Zotero/Markdown.pm
+++ lib/Zotero/Markdown.pm
@@ -4,42 +4,8 @@ use Moo;
 
 use MozRepl;
 
-=head1 NAME
-
-Zotero::Markown
-
-=head2 DESCRIPTION
-
-Package for handling human readable Author/Date citations in markdown format.
-Designed to be good enough for common use cases, not perfect.
-
-
-=head2 SYNOPSIS
-
-Will scan a plain text document paragraph by paragraph for citations using
-a human readableformat to key citations.  Conversion will die without
-modifying the document if there are ambiguous citation keys.
-
-Examples of the format are
-
- (c|{10.1108/14691930310504536} Fletcher 2003 Mapping stakeholder perceptions)
- (c|Law 2008 On sociology)
-
-You can put perl regex elements into the title portion.  e.g. ^ $ .* etc.
-
-=cut
-
-=head2 citations
-
-hashref of citations already seen during document processing, keyed by the
-citation text provided by the user.
-
-=cut
-
 has citations => (is => 'ro', default => sub {{}} );
 
-
-# mozrepl object with zotero object in scope.
 
 has repl => (is => 'ro', lazy => 1, builder     => '_build_repl',);
 
@@ -51,20 +17,10 @@ sub _build_repl {
     return $repl;
 }
 
-=head2 _run
-
-sends javascript commands to the repl and returns the result.
-
-=cut
-
 sub _run {
     my ($self, @commands) = @_;
     $self->repl->execute($_) for @commands;
 }
-
-=head2 parse_citation
-
-=cut
 
 sub parse_citation {
     my ($self, $cite ) = @_;
@@ -79,14 +35,6 @@ sub parse_citation {
 }
 
 
-=head2 citation_regex
-
-Simple regex for parsing the text string.
-TODO.  consider making a proper parser.
-TODO.  consider optional doi support.
-
-=cut.
-
 has citation_regex => ( is => 'ro',
                         default => sub {
                             qr/\(c\|               # preamble
@@ -94,5 +42,57 @@ has citation_regex => ( is => 'ro',
                                (?<year>\d+)\s+    # year
                                (?<title>.*?)\)/x; # title fragment
                         });
+
+
+
 1;
 
+__END__
+=head1 NAME
+
+Zotero::Markdown
+
+=head2 DESCRIPTION
+
+Package for handling human readable Author/Date citations in markdown format.
+Designed to be good enough for common use cases, not perfect.
+
+=head2 SYNOPSIS
+
+Will scan a plain text document paragraph by paragraph for citations using
+a human readable format to key citations.  Conversion will die without
+modifying the document if there are ambiguous citation keys.
+
+Examples of the format are
+
+ (c|Fletcher 2003 Mapping stakeholder perceptions)
+ (c|Law 2008 On sociology)
+
+You can put perl regex elements into the title portion.  e.g. ^, $,
+.*
+
+=head2 citations
+
+hashref of citations seen during document processing, keyed by the
+citation text provided by the user.  Used to memoize
+
+=head2 repl
+
+MozRepl object for internal use
+
+=head2 _run
+
+sends javascript commands to the repl and returns the result.
+
+=head2 parse_citation
+
+parses the citation to author title and year hashref
+
+=head2 citation_regex
+
+Simple regex for parsing the text string.
+TODO.  consider making a proper parser.
+TODO.  consider optional doi support.
+
+
+ 
